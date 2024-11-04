8e83bdf4d43b377cbdb918c801945c0658c5b7a6
Daffa Hanifisyafiq <daffa@mkpmobile.com>
2024-11-04 21:48:50 +0700

    feat: audio backsound

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,41 @@
 import { Outlet } from "react-router-dom";
-import { icons } from "../../constant/icons";
+import { useEffect, useRef } from "react";
 
 const Layout = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    const playAudio = () => {
+      if (audioRef.current) {
+        audioRef.current.play().catch((error) => {
+          console.log("Autoplay prevented:", error);
+        });
+      }
+    };
+
+    // Tambahkan event listener di window
+    window.addEventListener("click", playAudio, { once: true });
+    window.addEventListener("touchstart", playAudio, { once: true });
+
+    return () => {
+      window.removeEventListener("click", playAudio);
+      window.removeEventListener("touchstart", playAudio);
+    };
+  }, []);
+
   return (
     <>
-      <div className="tablet:hidden flex h-screen w-full flex-col items-center justify-center bg-black">
+      <audio ref={audioRef} src="/public/backsound.mp3" loop className="hidden">
+        Your browser does not support the audio element.
+      </audio>
+
+      <div className="flex h-screen w-full flex-col items-center justify-center bg-black tablet:hidden">
         <div className="phone"></div>
         <div className="mt-10">
           <p className="text-white">Please rotate your device!</p>
         </div>
       </div>
-      <div className="tablet:block hidden">
+      <div className="hidden tablet:block">
         <Outlet />
       </div>
     </>
