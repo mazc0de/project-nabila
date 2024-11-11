47e18ded938b06d11ce8092e25360d67a6956dfa
Daffa Hanifisyafiq <daffa@mkpmobile.com>
2024-11-12 00:20:17 +0700

    feat: main menu page

--- src/constant/images.ts
+++ src/constant/images.ts
@@ -2,8 +2,8 @@ import SCHOOL from "../assets/images/school.png";
 import TEACHER from "../assets/images/teacher.png";
 import STUDENTS from "../assets/images/student.png";
 import STAR_LOGO from "../assets/images/logo/star_logo.png";
-import QUIZ_BUTTON from "../assets/icons/quiz-button.png";
-import MATERIAL_BUTTON from "../assets/icons/material-button.png";
+import QUIZ_BUTTON from "../assets/images/button_quiz.png";
+import MATERIAL_BUTTON from "../assets/images/button_material.png";
 
 export const images = {
   SCHOOL,
--- src/pages/MainMenu/index.tsx
+++ src/pages/MainMenu/index.tsx
@@ -2,34 +2,23 @@ import { images } from "../../constant/images";
 
 const MainMenu = () => {
   return (
-    <div className="bg-main-menu flex h-screen w-full overflow-hidden bg-cover bg-center">
-      <div className="flex w-1/2 justify-center">
-        <div className="relative h-screen w-3/4">
-          <img
-            src={images.STAR_LOGO}
-            alt="star-logo"
-            className="bounce-image mt-5 w-96 tablet:w-[450px] laptop:w-[550px]"
-          />
-          <img
-            src={images.TEACHER}
-            alt="teacher-image"
-            className="absolute bottom-0 left-1/2 w-96 -translate-x-1/2 transform tablet:w-56"
-          />
-        </div>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-5 bg-main-menu bg-cover bg-bottom laptop:gap-10 laptop:bg-center">
+      <div className="w-56 laptop:w-96">
+        <p className="font-moresugar-regular text-center text-2xl laptop:text-5xl laptop:leading-[60px]">
+          Welcome to my English Class !
+        </p>
       </div>
-      <div className="flex w-1/2 items-center justify-center gap-10">
-        <img
-          src={images.QUIZ_BUTTON}
-          alt="quiz-button"
-          className="w-56 cursor-pointer duration-300 ease-in-out hover:scale-110 active:opacity-50"
-        />
+      <div className="flex gap-10">
         <img
           src={images.MATERIAL_BUTTON}
           alt="material-button"
-          className="w-56 cursor-pointer duration-300 ease-in-out hover:scale-110 active:opacity-50"
+          className="h-36 w-36 cursor-pointer duration-300 ease-in-out hover:scale-110 laptop:h-52 laptop:w-52"
         />
-        {/* <div className="bg-off-white-100 shadow-card h-[280px] w-[280px] rounded-2xl"></div>
-        <div className="bg-off-white-100 shadow-card h-[280px] w-[280px] rounded-2xl"></div> */}
+        <img
+          src={images.QUIZ_BUTTON}
+          alt="material-button"
+          className="h-36 w-36 cursor-pointer duration-300 ease-in-out hover:scale-110 laptop:h-52 laptop:w-52"
+        />
       </div>
     </div>
   );
