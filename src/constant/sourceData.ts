4cf9db5fac906f42d2c77d29ced377297931bc95
Daffa Hanifisyafiq <daffa@mkpmobile.com>
2025-02-09 14:30:01 +0700

    feat: source page

---
+++ src/constant/sourceData.ts
@@ -0,0 +1,49 @@
+export const sourceData = [
+  {
+    id: 1,
+    title: "Material",
+    list: [
+      {
+        id: 1,
+        label: "Modul Ajar Kelas X",
+      },
+      {
+        id: 2,
+        label:
+          "Gerot, L. & Wignell, P. (1994). Making sense of functional grammar. Gerd Stabler, AEE Publishing.",
+      },
+      {
+        id: 3,
+        label:
+          "Knapp, P. & Watkins, M. (2005). Genre, text, grammar : technologies for teaching and assessing writing. UNSW Press.",
+      },
+      {
+        id: 4,
+        label:
+          "Wijayatiningsih, T. D., & Wilujeng, A. (2015). Bahan ajar genre-based writing. CV Rafi Sarana Perkasa.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Assets (image, music, etc..)",
+    list: [
+      {
+        id: 1,
+        label: "canva.com",
+      },
+      {
+        id: 2,
+        label: "pinterest.com",
+      },
+      {
+        id: 3,
+        label: "freepik.com",
+      },
+      {
+        id: 4,
+        label: "pixabay.com",
+      },
+    ],
+  },
+];
--- src/pages/Setting/Source/index.tsx
+++ src/pages/Setting/Source/index.tsx
@@ -1,5 +1,28 @@
+import { sourceData } from "@/constant/sourceData";
+
 const Source = () => {
-  return <div>Source</div>;
+  return (
+    <div>
+      {sourceData.map((item) => {
+        return (
+          <div className="last:mt-2">
+            <p className="font-moreSugar text-base font-bold" key={item?.id}>
+              {item?.title}
+            </p>
+            <ul className="ml-6 list-disc">
+              {item?.list?.map((item) => {
+                return (
+                  <li key={item?.id}>
+                    <p className="font-moreSugar text-base">{item?.label}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Source;
