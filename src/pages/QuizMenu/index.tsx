1b879fbdb18026c3997a276273acd5e8eea45938
Daffa Hanifisyafiq <daffa@mkpmobile.com>
2025-02-13 23:41:17 +0700

    refactor: remove unused code

--- src/pages/QuizMenu/index.tsx
+++ src/pages/QuizMenu/index.tsx
@@ -41,46 +41,6 @@ const QuizMenu = () => {
         </div>
       </div>
     </div>
-    // <div className="relative flex h-screen w-full items-center justify-center bg-quiz-menu bg-cover bg-bottom">
-    //   <div>
-    //     <Link to="/main-menu">
-    //       <img
-    //         src={icons.BUTTON_HOME}
-    //         alt="home-button"
-    //         className="button-effect-clicked absolute left-5 top-5 w-10"
-    //       />
-    //     </Link>
-    //   </div>
-    //   <div className="flex h-full flex-col items-center justify-center gap-8">
-    //     <p className="text-center font-moreSugar text-4xl font-bold">QUIZ</p>
-    //     <div className="flex gap-8">
-    //       <Link to="/quiz/multiple-choice/1">
-    // <div className="flex h-full w-full flex-col items-center justify-center gap-2">
-    //   <img
-    //     src={icons.QUIZ_MULTIPLE_CHOICE}
-    //     alt="material-button"
-    //     className="button-effect-clicked h-28 w-28 laptop:h-36 laptop:w-3h-36"
-    //   />
-    //   <p className="text-center font-moreSugar text-lg font-bold text-[#FACCC2]">
-    //     MULTIPLE CHOICE
-    //   </p>
-    // </div>
-    //       </Link>
-    //       <Link to="/quiz/true-or-false">
-    //         <div className="flex h-full w-full flex-col items-center justify-center gap-2">
-    //           <img
-    //             src={icons.QUIZ_TRUE_OR_FALSE}
-    //             alt="material-button"
-    //             className="button-effect-clicked h-28 w-28 laptop:h-36 laptop:w-3h-36"
-    //           />
-    //           <p className="text-center font-moreSugar text-lg font-bold text-[#FACCC2]">
-    //             TRUE OR FALSE
-    //           </p>
-    //         </div>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
